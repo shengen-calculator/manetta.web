fe82e8417057fbf7dd98d81fe613b1bb547564eb
Bass <suddya@toptal.com>
2023-07-09 08:46:09 +0300

    tags field based on -> SELECT element

--- src/page/operations/TagDialog.tsx
+++ src/page/operations/TagDialog.tsx
@@ -6,6 +6,39 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import {
+    Box,
+    Checkbox, Chip, FormControl,
+    FormControlLabel,
+    InputLabel, MenuItem,
+    OutlinedInput,
+    Select,
+    SelectChangeEvent
+} from "@mui/material";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+const names = [
+    'Oliver Hansen',
+    'Van Henry',
+    'April Tucker',
+    'Ralph Hubbard',
+    'Omar Alexander',
+    'Carlos Abbott',
+    'Miriam Wagner',
+    'Bradley Wilkerson',
+    'Virginia Andrews',
+    'Kelly Snyder',
+];
 
 interface TagDialogProps {
     isOpen: boolean
@@ -25,25 +58,73 @@ const TagDialog: React.FC<TagDialogProps> = (
         save();
     };
 
+
+    const [personName, setPersonName] = React.useState<string[]>([]);
+
+    const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+        const {
+            target: { value },
+        } = event;
+        setPersonName(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+
     return (
         <div>
             <Dialog
                 open={isOpen}
                 onClose={onCancel}>
-                <DialogTitle>Subscribe</DialogTitle>
+                <DialogTitle>Apply Tags</DialogTitle>
                 <DialogContent>
                     <DialogContentText>
-                        To subscribe to this website, please enter your email address here. We
-                        will send updates occasionally.
+                        Don't forget that tag order is important for reports. Please find more information in provided documentation..
                     </DialogContentText>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-multiple-chip-label">Tags</InputLabel>
+                        <Select
+                            labelId="demo-multiple-chip-label"
+                            id="demo-multiple-chip"
+                            multiple
+                            value={personName}
+                            onChange={handleChange}
+                            input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                            renderValue={(selected) => (
+                                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                                    {selected.map((value) => (
+                                        <Chip key={value} label={value} />
+                                    ))}
+                                </Box>
+                            )}
+                            MenuProps={MenuProps}
+                        >
+                            {names.map((name) => (
+                                <MenuItem
+                                    key={name}
+                                    value={name}
+                                >
+                                    {name}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
                     <TextField
                         autoFocus
                         margin="dense"
                         id="name"
-                        label="Email Address"
-                        type="email"
+                        label="Extra"
+                        type="text"
                         fullWidth
-                        variant="standard"
+                    />
+                    <FormControlLabel control={<Checkbox defaultChecked={false} />} label="Save as group" />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Group"
+                        type="text"
+                        fullWidth
                     />
                 </DialogContent>
                 <DialogActions>
